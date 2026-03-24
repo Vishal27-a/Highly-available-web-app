799c6597eff3d2711ec9c6ae3f98a312b5a94b12
Vishal Kumar Maurya <vishalmaurya2551@gmail.com>
2026-03-24 22:52:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,1 @@
-# backend config add
-terraform {
-  backend "s3" {
-    bucket         = "vishal-terraform-state-12345"
-    key            = "prod/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
+
